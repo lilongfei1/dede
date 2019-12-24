9418469a051a7bcd032b6224eba6907899a19646
lilongfei1 <3282156259@qq.com>
2019-12-24 10:20:02 +0800

    1

---
+++ src/com/imti/dd/User.java
@@ -0,0 +1,15 @@
+package com.imti.dd;
+
+public class User {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+
+}
